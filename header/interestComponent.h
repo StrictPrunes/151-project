91478b7f3068e26f6bcf662bf19b0452d83224b5
StrictPrunes <jeromey07mcg@gmail.com>
2025-12-09 22:46:27 -0800

    Create interestComponent.h

---
+++ header/interestComponent.h
@@ -0,0 +1,14 @@
+/****************
+ * Simon Fraser University
+ * ENSC151 Intro to Software Development for Engineers
+ * interestComponent.h -- Program that calculates the interest gained on savings
+ * 
+ * Input: The principal (initial) amount
+ *            Annual interest rate
+ *            Number of times interest is compounded
+ * Output: The amount of interest earned
+ * 
+ * Author: Jonathan Chen
+ *****************/
+
+void interest(double savingValue);
