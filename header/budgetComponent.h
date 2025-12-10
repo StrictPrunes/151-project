a8e8b0563fdb9fac1ebb018f70ee6d8f77e2e1e7
StrictPrunes <jeromey07mcg@gmail.com>
2025-12-09 22:47:30 -0800

    Create budgetComponent.h

---
+++ header/budgetComponent.h
@@ -0,0 +1,15 @@
+ /******************************************************************
+ * Simon Fraser University
+ * ENSC-151 Introduction to Software Development for Engineers
+ * budgetComponent.h -- prompts and computes user budget
+ * 
+ * Input: -
+ * 
+ * Output: 
+ *
+ * Authors: Jeromey Gong and Gabriel Dryer
+ *
+ *******************************************************************/
+void budget (double netIncome, double &savingValue);
+void recommendedBudget (double totalBudget, double &savingValue);
+void customBudget (double totalBudget, double &savingValue);
